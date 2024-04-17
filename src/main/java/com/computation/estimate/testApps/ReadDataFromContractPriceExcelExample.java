a431f4ae12636a830af76ed146497b947d4b3deb
Serhii Karpusha <kapcep@gmail.com>
2024-04-17 21:33:31 +0300

    add possibility to get computationPositionContractPrice from ContractPriceExcel file

--- src/main/java/com/computation/estimate/testApps/ReadDataFromContractPriceExcelExample.java
+++ src/main/java/com/computation/estimate/testApps/ReadDataFromContractPriceExcelExample.java
@@ -3,6 +3,8 @@ package com.computation.estimate.testApps;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -11,7 +13,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-public class ReadDataFromExcelExample {
+public class ReadDataFromContractPriceExcelExample {
 	public static void main(String[] args) {
 		try (Workbook workbook = WorkbookFactory
 				.create(new FileInputStream("files/U_392_ДЦ_КК_2.xls"))) {
@@ -19,33 +21,32 @@ public class ReadDataFromExcelExample {
 			DecimalFormat df = new DecimalFormat("#,##0.00");
 
 			int count = 1;
+			Map<Integer, Double> computationPositionContractPriceMap = new HashMap<>();
 
 			for (int i = 14; i <= sheet.getLastRowNum(); i++) {
 				Row row = sheet.getRow(i);
-
 				Cell firstCell = row.getCell(0);
 
 				if (row != null && firstCell.getCellType() == CellType.NUMERIC
 						&& (int) firstCell.getNumericCellValue() == count) {
 
-					Cell computationPositionNameCell = row.getCell(2);
-					Cell computationPositionCell = row.getCell(8);
-					System.out
-							.println(
-									"(" + count + ") "
-											+ computationPositionNameCell
-													.getStringCellValue()
-													.replaceAll("\\s+", " ")
-											+ " ціна: "
-											+ df.format(computationPositionCell
-													.getNumericCellValue()
-													* 1.2)
-											+ "грн.");
+					Cell computationPositionContractPriceCell = row.getCell(8);
+
+					Double computationPositionContractPrice = Double.valueOf(df
+							.format(computationPositionContractPriceCell
+									.getNumericCellValue() * 1.2)
+							.replaceAll(",", ".").replaceAll(" ", ""));
+
+					computationPositionContractPriceMap.put(count,
+							computationPositionContractPrice);
 					count++;
 				}
 			}
+			computationPositionContractPriceMap
+					.forEach((k, v) -> System.out.println(k + " : " + v));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
