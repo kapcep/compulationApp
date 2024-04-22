3e4e87f74e62a90cf528b7e5566ed3eab2d7dbcd
Serhii Karpusha <kapcep@gmail.com>
2024-04-22 18:15:32 +0300

    1. added vertical and horizontal alignment to style. 2. changed height of first row. 3. added third row. 4. added numbering in third row.

--- src/main/java/com/computation/estimate/service/OperationalInfoExcelFileCreator.java
+++ src/main/java/com/computation/estimate/service/OperationalInfoExcelFileCreator.java
@@ -9,6 +9,7 @@ import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -37,19 +38,27 @@ public class OperationalInfoExcelFileCreator {
 
 		// centerAlignStyleAndBoldFont
 
-		CellStyle centerAlignStyleAndBoldFont = workbook.createCellStyle();
-		centerAlignStyleAndBoldFont.setAlignment(HorizontalAlignment.CENTER);
+		CellStyle centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont = workbook
+				.createCellStyle();
+		centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont
+				.setAlignment(HorizontalAlignment.CENTER);
+		centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont
+				.setVerticalAlignment(VerticalAlignment.CENTER);
+		centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont
+				.setWrapText(true);
 
 		Font fontBold = workbook.createFont();
 		fontBold.setBold(true);
 		fontBold.setFontHeightInPoints((short) 12);
 		// fontBold.setFontName("Times New Roman");
-		centerAlignStyleAndBoldFont.setFont(fontBold);
+		centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont
+				.setFont(fontBold);
 
 		// fontWith10heightStyle
 
 		CellStyle fontWith10HeightStyle = workbook.createCellStyle();
 		fontWith10HeightStyle.setAlignment(HorizontalAlignment.CENTER);
+		fontWith10HeightStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 		Font fontWith10height = workbook.createFont();
 		fontWith10height.setFontHeightInPoints((short) 10);
 		fontWith10HeightStyle.setFont(fontWith10height);
@@ -58,9 +67,11 @@ public class OperationalInfoExcelFileCreator {
 
 		// row one
 		Row row0 = sheet.createRow(0);
+		row0.setHeight((short) 900);
 		Cell cell00 = row0.createCell(0);
-		cell00.setCellValue("ВІДОМІСТЬ ОБ`ЄМІВ РОБІТ");
-		cell00.setCellStyle(centerAlignStyleAndBoldFont);
+		cell00.setCellValue("НАЗВА ОБЄКТА");
+		cell00.setCellStyle(
+				centerAlignStyleAndBoldAndHorizontalAlignmentAndVerticalAlignmanetFont);
 
 		// row two
 		Row row1 = sheet.createRow(1);
@@ -100,10 +111,20 @@ public class OperationalInfoExcelFileCreator {
 		sheet.setColumnWidth(7, 160 * 30);
 		sheet.setColumnWidth(8, 100 * 30);
 
-		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 1));
+		// union cells
+		sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 9));
 		sheet.addMergedRegion(new CellRangeAddress(1, 1, 3, 5));
 		sheet.addMergedRegion(new CellRangeAddress(1, 1, 6, 7));
 		sheet.addMergedRegion(new CellRangeAddress(1, 1, 8, 9));
 
+		// row 3
+		Row row2 = sheet.createRow(2);
+
+		// add horizontal numbering
+		for (int i = 1; i <= 10; i++) {
+			Cell cell = row2.createCell(i - 1);
+			cell.setCellStyle(fontWith10HeightStyle);
+			cell.setCellValue(i);
+		}
 	}
 }
